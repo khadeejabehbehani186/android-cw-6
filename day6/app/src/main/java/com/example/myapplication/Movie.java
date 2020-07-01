9875ae93ee1b34a95c2cff46f67c6f14f16c3ba0
khadeejabehbehani186 <66696471+khadeejabehbehani186@users.noreply.github.com>
2020-07-01 17:52:33 +0300

    done

---
+++ day6/app/src/main/java/com/example/myapplication/Movie.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+
+public class Movie {
+    private String title ;
+    private String mainActor ;
+    private Double movieRate ;
+    private int pgRate ;
+    private String genre ;
+}
